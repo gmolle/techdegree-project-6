07868b1c2370f3790ac8c7458129db8eaf437541
Garrett Molle <46017064+gmolle@users.noreply.github.com>
2019-07-22 21:43:43 -0400

    added procfile

--- app.js
+++ app.js
@@ -54,6 +54,9 @@ app.use((err, req, res, next) => {
   res.render('error')
 })
 
-app.listen(3000, () => {
-  console.log('The application is running on localhost:3000');
+let port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`The application is running on localhost:${port}`);
 });
